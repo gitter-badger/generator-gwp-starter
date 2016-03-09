4c9f871af9e66918416b66eae44f5f76a2f59d81
Covington Doan <covington.doan@gmail.com>
2016-03-09 09:46:14 -0600

    Add prompts and Craft install

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,8 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
+var craftUrl = 'https://craftcms.com/latest.zip?accept_license=yes';
+
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
     var done = this.async();
@@ -13,25 +15,95 @@ module.exports = yeoman.generators.Base.extend({
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name: 'projectName',
+      message: 'What\'s the name of your project?',
+      default: 'GW Starter'
+    }, {
+      name: 'dbLocalUrl',
+      message: '[LOCAL] Domain of your local installation',
+      default: 'gw-starter.dev'
+    }, {
+      name: 'dbLocalServer',
+      message: '[LOCAL] Name of your local database server',
+      default: 'localhost'
+    }, {
+      name: 'dbLocalUser',
+      message: '[LOCAL] Name of your local database user',
+      default: 'root'
+    }, {
+      name: 'dbLocalPassword',
+      message: '[LOCAL] Password of your local database user',
+      default: 'root'
+    }, {
+      name: 'dbLocalName',
+      message: '[LOCAL] Name of your local database'
+    }, {
+      name: 'dbStagingUrl',
+      message: '[STAGING] Domain of your staging installation'
+    }, {
+      name: 'dbStagingServer',
+      message: '[STAGING] Name of your staging database server'
+    }, {
+      name: 'dbStagingUser',
+      message: '[STAGING] Name of your staging database user'
+    }, {
+      name: 'dbStagingPassword',
+      message: '[STAGING] Password of your staging database user'
+    }, {
+      name: 'dbStagingName',
+      message: '[STAGING] Name of your staging database'
     }];
 
     this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+      this.projectName = props.projectName;
+
+      this.dbLocalUrl = props.dbLocalUrl;
+      this.dbLocalServer = props.dbLocalServer;
+      this.dbLocalUser = props.dbLocalUser;
+      this.dbLocalPassword = props.dbLocalPassword;
+      this.dbLocalName = props.dbLocalName;
+
+      this.dbStagingUrl = props.dbStagingUrl;
+      this.dbStagingServer = props.dbStagingServer;
+      this.dbStagingUser = props.dbStagingUser;
+      this.dbStagingPassword = props.dbStagingPassword;
+      this.dbStagingName = props.dbStagingName;
 
       done();
     }.bind(this));
   },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    var projectName = {
+      name: this.projectName
+    };
+    var dbLocal = {
+      server: this.dbLocalServer,
+      user: this.dbLocalUser,
+      password: this.dbLocalPassword,
+      dbname: this.dbLocalName
+    };
+    var dbStaging = {
+      server: this.dbStagingServer,
+      user: this.dbStagingUser,
+      password: this.dbStagingPassword,
+      dbname: this.dbStagingName
+    };
+    var domains = {
+      local: this.dbLocalUrl,
+      staging: this.dbStagingUrl
+    };
+
+    // Download and extract Craft
+    var done = this.async();
+    this.extract(craftUrl, '.', {mode: '755'}, function(error) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('extracted');
+            done();
+        }
+    });
   },
 
   install: function () {
